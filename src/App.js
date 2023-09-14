0a93cae3823a6f3926f2a1ddce2e5e494132969e
kimberlykpe <108425536+kimberlykpe@users.noreply.github.com>
2023-09-14 18:59:13 -0400

    adding api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect } from 'react';
 
-function App() {
+//4d2b405a
+//commit
+const APT_URL = 'http://www.omdbapi.com?apikey=4d2b405a';
+
+const App = () => {
+
+  const searchMovies = async (title) => {
+    const response = await fetch(`$(API_URL)&s=${title}`);
+    const data = await response.json();
+
+    console.log(data.Search);
+  }
+  useEffect (() => {
+      searchMovies('Spiderman');
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Movie-Search</h1>
     </div>
   );
 }
