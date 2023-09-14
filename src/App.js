207ab85cdaf40427d601ed77b68de14ee278dc1f
kimberlykpe <108425536+kimberlykpe@users.noreply.github.com>
2023-09-14 19:29:28 -0400

    create info for movie card

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
 import { useEffect } from 'react';
-
+import SearchIcon from './search.svg';
 //4d2b405a
-//commit
-const APT_URL = 'http://www.omdbapi.com?apikey=4d2b405a';
 
+const API_URL = 'http://www.omdbapi.com?apikey=4d2b405a';
+
+const movie1 = {
+  "Title": "Amazing Spiderman Syndrome",
+  "Year": "2012",
+  "imdbID": "tt2586634",
+  "Type": "movie",
+  "Poster": "N/A"
+}
 const App = () => {
 
   const searchMovies = async (title) => {
@@ -18,8 +25,30 @@ const App = () => {
   }, [])
 
   return (
-    <div className="App">
+    <div className="app">
       <h1>Movie-Search</h1>
+      <div className="search">
+        <input 
+         placeholder='Search for movies'
+         value=""
+         onChange={() => {}} />
+        <img 
+          src ={SearchIcon} 
+          alt="search"
+          onClick={() => {}}/>
+      </div>
+      
+      <div className="container">
+        <div className="movie">
+          <div>
+            <p>{movie1.Year}</p>
+          </div>
+
+          <div>
+            <img src={movie1.Poster !== 'N/A' ? movie1.Poster : 'https://via.placeholder.com/400'} alt ="movie img" />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
