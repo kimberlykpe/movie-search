215d85f8295452225d5b83679f41945799b6572c
kimberlykpe <108425536+kimberlykpe@users.noreply.github.com>
2023-09-14 20:05:23 -0400

    update movie hover titl and category

--- src/MovieCard.jsx
+++ src/MovieCard.jsx
@@ -11,8 +11,10 @@ const MovieCard = ( { movie } ) => {
             <img src={movie.Poster !== 'N/A' ? movie.Poster : 'https://via.placeholder.com/400'} alt ="movie img" />
         </div>
 
+        <div>
         <span>{movie.Type}</span>
         <h3>{movie.Title}</h3>
+        </div>
      </div>
   )
 }
